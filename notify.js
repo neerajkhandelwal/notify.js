8baf6c16cceed451d9bb227219c45d615e9044cb
Neeraj Khandelwal <nk.zscorpion@gmail.com>
2012-05-20 04:02:21 +0530

    Version 1.0

--- notify.js
+++ notify.js
@@ -2,7 +2,7 @@
 /* Notify is a jQuery plugin to display notification. For example, if there is a update on the website and you want the user to know it. You can notify her by calling notify function of the plugin. It will display the notification at the your desired position.*/
 
 $.fn.notify = function(){
-	$(this).css({'display':'none','background-color':'#222','font-face':'Halvetica', 'font-size':'18x','font-weight':'bolder','color':'#DDD','position':'absolute','width':'auto','right':'30px','top':'50px','font-align':'center','padding':'15px','z-index':'1000','opacity':'.7','-moz-border-radius':'10px','-webkit-border-radius':'10px','text-shadow':'0px 1px 0px 1px'});
+	$(this).css({'display':'none','background-color':'#222','font-face':'Halvetica', 'font-size':'18x','font-weight':'bolder','color':'#FFF','position':'absolute','width':'auto','right':'30px','top':'50px','font-align':'center','padding':'15px','z-index':'1000','opacity':'.8','-moz-border-radius':'10px','-webkit-border-radius':'10px','text-shadow':'0px 1px 0px 1px'});
 	if(typeof(arguments[1]) != "undefined"){
 		$(this).css(arguments[1]);
 	};
